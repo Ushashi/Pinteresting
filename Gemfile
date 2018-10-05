8c6d9c2da6fc0344df18d13d5c78d7e64b3bdb98
Ushashi <uchaky@gmail.com>
2018-10-05 00:23:39 -0700

    Adding uploading of images functionality

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.5.0'
+gem 'paperclip', '~> 3.0'
 
 group :development, :test do
      gem 'sqlite3'
---
+++ db/migrate/2018100505050784_add_attachment_image_to_pins.rb
@@ -0,0 +1,11 @@
+class AddAttachmentImageToPins < ActiveRecord::Migration[5.2]
+  def self.up
+  	change_table :pins do |t|
+  		t.attachment :image
+  	end
+  end
+
+  def self.down
+  	drop_attached_file :pins, :image
+  end
+end
